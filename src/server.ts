35e8379a77f5440f894da4249b06ad5a27aad72d
Nicolas <devnicolasmateus@gmail.com>
2026-08-03 22:59:14 -0300

    c9 - version 1 - fixing server port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app.js";
 import "dotenv/config";
 
-app.listen (3001, () => {
+const port = process.env.PORT || 3001;
+
+app.listen (port, () => {
     console.log ("Running!")
-})
+}); 
